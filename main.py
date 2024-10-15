de7a8cf23573b15fdcf4185c5bf37e9b4041a2f6
Rodion1144 <chistyakov-rodion@bk.ru>
2024-10-15 16:40:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+list = []
+n = 0
+while n < len(my_list) and my_list[n] > 0:
+    list.append(my_list[n])
+    n += 1
+print(list)
